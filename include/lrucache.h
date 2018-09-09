173bd81cf0f63c20efb07ea04b9713a2e6982227
Pourya Shirazian <Pourya.Shirazian@gmail.com>
2018-09-08 22:15:03 -0700

    test passed

--- include/lrucache.h
+++ include/lrucache.h
@@ -1,6 +1,7 @@
 #include <utility>
 #include <list>
 #include <unordered_map>
+#include <atomic>
 
 using namespace std;
 
@@ -25,7 +26,7 @@ protected:
  * in the next available memory slot. In case no memory slots are available, the least recently 
  * used item is removed from the list to accomodate the new value.
  */ 
-template < typename KeyType, typename ValueType, typename RegisterPolicy>
+template < typename KeyType, typename ValueType, typename RegisterPolicy = DefaultRegisterPolicy>
 class LRUCache : private RegisterPolicy {
     using RegisterPolicy::Register;
     using RegisterPolicy::Unregister;
@@ -34,36 +35,61 @@ public:
   LRUCache(unsigned int size);
   virtual ~LRUCache();
 
+  void Clear();
+
   int Fetch(const KeyType& key, ValueType& output_value);
 
-protected:
-    unsigned int size_;
+  unsigned int GetSize() const;  
 
+  double GetHitRatio() const;
+  uint64_t GetFetchCount() const;
+  uint64_t GetHitCount() const;
+
+protected:
     typedef std::pair<KeyType, ValueType> KeyValuePairType;
     std::list< KeyValuePairType > queue_;
     std::unordered_map< KeyType, KeyValuePairType* > map_;    
+    std::atomic<uint64_t> fetch_count_;
+    std::atomic<uint64_t> hit_count_;
+
+    unsigned int size_;
 };
 
 
 template < typename KeyType, typename ValueType, typename RegisterPolicy>
-LRUCache<KeyType, ValueType, RegisterPolicy>::LRUCache(unsigned int size): size_(size) {
+LRUCache<KeyType, ValueType, RegisterPolicy>::LRUCache(unsigned int size): size_(size), fetch_count_(0), hit_count_(0) {
 
 }
 
 template < typename KeyType, typename ValueType, typename RegisterPolicy>
 LRUCache<KeyType, ValueType, RegisterPolicy>::~LRUCache() {
+    Clear();
+}
+
+template < typename KeyType, typename ValueType, typename RegisterPolicy>
+void LRUCache<KeyType, ValueType, RegisterPolicy>::Clear() {
     map_.clear();
     queue_.clear();
+    fetch_count_ = 0;
+    hit_count_ = 0;    
 }
 
 template < typename KeyType, typename ValueType, typename RegisterPolicy>
 int LRUCache<KeyType, ValueType, RegisterPolicy>::Fetch(const KeyType& key, ValueType& output_value) {
+    fetch_count_ ++;
 
     //check if the key is NOT present in the map. Then register it
     auto it = map_.find(key);
     if(it == map_.end()) {
+
+        //use the supplied register policy to perform value computation
         output_value = Register(key);
+
+        //if we are FULL then remove the LRU item
         if(queue_.size() == size_) {
+            //Unregister the LRU item
+            Unregister(queue_.back().first, queue_.back().second);
+
             //remove LRU item from map by key
             map_.erase(queue_.back().first);
 
@@ -76,7 +102,28 @@ int LRUCache<KeyType, ValueType, RegisterPolicy>::Fetch(const KeyType& key, Valu
         return 1;
     }
     else {
+        hit_count_ ++;
         output_value = (*it).second->second;
         return 0;
     }
+}
+
+template < typename KeyType, typename ValueType, typename RegisterPolicy>
+unsigned int LRUCache<KeyType, ValueType, RegisterPolicy>::GetSize() const {
+    return size_;
+}
+
+template < typename KeyType, typename ValueType, typename RegisterPolicy>
+double LRUCache<KeyType, ValueType, RegisterPolicy>::GetHitRatio() const {
+    return static_cast<double>(hit_count_) / ((fetch_count_ == 0) ? 1.0 : static_cast<double>(fetch_count_));
+}
+
+template < typename KeyType, typename ValueType, typename RegisterPolicy>
+uint64_t LRUCache<KeyType, ValueType, RegisterPolicy>::GetFetchCount() const {
+    return fetch_count_;
+}
+
+template < typename KeyType, typename ValueType, typename RegisterPolicy>
+uint64_t LRUCache<KeyType, ValueType, RegisterPolicy>::GetHitCount() const {
+    return hit_count_;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include "lrucache.h"
+#include <iostream>
+
 
 class PowerTwoRegisterPolicy
 {
@@ -20,11 +22,17 @@ int main(int argc, char* argv[]) {
     LRUCacheP2 cache(4);
     int r, cost = 0;
     
-    cost += cache.Fetch(2, r);
-    cost += cache.Fetch(2, r);
-    cost += cache.Fetch(3, r);
-    cost += cache.Fetch(3, r);
+    for(int i=0; i < 100000; i++) {
+        cost += cache.Fetch(2, r);
+        cost += cache.Fetch(2, r);
+        cost += cache.Fetch(3, r);
+        cost += cache.Fetch(3, r);
+    }
 
+    std::cout << "Cache Size = " << cache.GetSize() << std::endl;
+    std::cout << "Cache Total fetches = " << cache.GetFetchCount() << ", hit count = " << cache.GetHitCount() << std::endl;    
+    std::cout << "Cache Hit Ratio = " << cache.GetHitRatio() << std::endl;
+    std::cout << "Total access cost = " << cost << std::endl;
 
     exit(0);
 }
